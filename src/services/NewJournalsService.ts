93691190786bca1ba3a3b33278ad5ba88a921d91
Pavel Gnedov <pavel@gnedov.info>
2021-11-25 23:08:54 +0700

    dev

--- src/services/NewJournalsService.ts
+++ src/services/NewJournalsService.ts
@@ -1,12 +1,19 @@
 import {Service} from "@tsed/di";
 import {TimestampConverter} from "./fieldconverters/TimestampConverter";
+import {EnvsConsts} from "../consts/EnvsConsts";
 
 @Service()
 export class NewJournalsService {
 
+  private additionalSilenceTime: number = 0;
+
   constructor(
     private timestampConverter: TimestampConverter
-  ) {}
+  ) {
+    if (process.env[EnvsConsts.ADDITIONAL_SILENCE_TIME.toString()]) {
+      this.additionalSilenceTime = Number(process.env[EnvsConsts.ADDITIONAL_SILENCE_TIME.toString()] || 0)
+    }
+  }
 
   async getMessagesAfter(issue: any, after: any): Promise<any[]> {
     if (!(issue?.journals?.length > 0)) return [];
@@ -21,7 +28,7 @@ export class NewJournalsService {
     for (let i = 0; i < issue.journals.length; i++) {
       const journal = issue.journals[i];
       const journalTimestamp = await this.timestampConverter.convert(journal?.created_on || null) || -1;
-      if (journalTimestamp <= afterTimestamp) continue;
+      if (journalTimestamp <= afterTimestamp + this.additionalSilenceTime) continue;
       newJournals.push(journal);
     }
     return newJournals;
